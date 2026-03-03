16326a67bd298b784ec4a1998cf8e831c545dacf
Hridhya Narayanan <hnarayanan.ext@libertyglobal.com>
2026-03-03 03:41:02 +0000

    [ONEM-43335]: Enable PSON-Process Swap on Navigation mode

--- WebKitBrowser/WebKitBrowser.cpp
+++ WebKitBrowser/WebKitBrowser.cpp
@@ -339,8 +339,8 @@ namespace Plugin {
 
     void WebKitBrowser::ProcessSwapped()
     {
-        TRACE(Trace::Information, (_T("Inside ProcessSwapped()")));
-        _service->Notify(message);
+        TRACE(Trace::Information, (_T("ProcessSwapped: \"true\"")));
+        _service->Notify(_T("{\"ProcessSwapped\": true }");
         Exchange::JWebBrowser::Event::ProcessSwapped(*this);
     }
 
